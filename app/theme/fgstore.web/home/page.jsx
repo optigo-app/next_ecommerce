8e43e2102a455c74a726666f84880e57bd38cb27
Rajan <optigoapi@gmail.com>
2025-09-16 14:50:39 +0530

    unknown changes

--- app/theme/fgstore.web/home/page.jsx
+++ app/theme/fgstore.web/home/page.jsx
@@ -9,10 +9,12 @@ export const metadata = generatePageMetadata(pages['/'], 'Sonasons');
 const SonasonsHome = async () => {
     const storeData = await getStoreInit();
     return (
+        <>
         <div>
             Hello sonasons home page , this is the domain name getting from storeInit {storeData.domain}
         </div>
-        // <Header storeData={storeData} />
+        {/* <Header storeData={storeData} /> */}
+        </>
     )
 }
 
