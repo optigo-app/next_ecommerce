f58edaac6b8dbf41f9a2940468ffe18223562a4d
subrata <subrata@gmail.com>
2025-09-17 11:02:32 +0530

    product listing page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { getActiveTheme } from "./(core)/lib/getActiveTheme";
 
 export default async function Page() {
-  const theme = getActiveTheme();
-  const Product = (await import(`@/app/theme/${theme}/home/page.jsx`)).default;
-  return <Product />;
+  const theme = await getActiveTheme();
+  const Home = (await import(`@/app/theme/${theme}/home/page.jsx`)).default;
+  return <Home />;
 }
--- app/theme/fgstore.web/product/page.jsx
+++ app/theme/fgstore.web/product/page.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+// import React from 'react'
 
-const page = ({ params, searchParams }) => {
+// const page = ({ params, searchParams }) => {
+//     return (
+//         <div>
+//             Sonasons Products
+//             <pre>{JSON.stringify(params, null, 2)}</pre>
+//             <pre>{JSON.stringify(searchParams, null, 2)}</pre>
+//         </div>
+//     )
+// }
+
+// export default page;
+
+import Product from "./_prodComponents/page";
+import { getStoreInit } from "@/app/(core)/utils/GlobalFunctions/GlobalFunctions";
+
+const ProductList = async ({ params, searchParams }) => {
+    const storeinit = await getStoreInit();
+
     return (
-        <div>
-            Sonasons Products
-            <pre>{JSON.stringify(params, null, 2)}</pre>
-            <pre>{JSON.stringify(searchParams, null, 2)}</pre>
-        </div>
-    )
-}
+        <>
+            <Product params={params} searchParams={searchParams} storeinit={storeinit} />
+        </>
+    );
+};
 
-export default page
+export default ProductList;
+
+
